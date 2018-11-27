dff4dab692eef53d0f0426fcfd572083d4a70331
Juan Ignacio Suarez <juaniisuar@gmail.com>
2018-11-27 11:12:50 -0300

    Mini Parser to read from files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Functional-Dependencies
 C++ implementation of functional dependencies
+
+How to use:
+1. Compile: g++ -O3 *.cpp -o fd
+2. Use: fd file [-countFD]
+
+-countFD is optional, prints the cardinality of F+
--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,95 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 #include "funcdependency.h"
 
-int main() {
-    /** Set 1 **/
-    /*
-    Attr a{'A'}, b{'B'}, c{'C'}, d{'D'};
-    AttrDependencies F;
-    F[a].insert(b);
-    F[b].insert(Attrs{a,d});
-    F[Attrs{b,c}].insert(a);
+int main(int argc, char* argv[]) {
+    if(argc < 2){
+        std::cout << "Use: fd file [-countFD]" << std::endl;
+        return 0;
+    }
 
-    Attrs a_closure = {};
-    AttrDependencies s_closure = set_closure(Attrs{a,b,c,d}, F); // = 137
-    SetAttrs cand_keys = {};
-    */
+    freopen(argv[1], "r", stdin);
 
-    /** Set 2 **/
-    /*
-    Attr a{'A'}, b{'B'}, c{'C'}, d{'D'}, e{'E'}, f{'F'};
-    AttrDependencies F;
-    F[Attrs{a,b}].insert(c);
-    F[Attrs{b,d}].insert(Attrs{e,f});
+    std::string inp, inpaux;
+    char holder;
+    int status = 0;
 
-    Attrs a_closure = {};
-    AttrDependencies s_closure = set_closure(Attrs{a,b,c,d,e,f}, F); // = 1081
-    SetAttrs cand_keys = {};
-    */
+    Attrs R{}, A{};
+    AttrDependencies F{};
 
-    /** Set 3 **/
-    /*
-    Attr a{'A'}, b{'B'}, c{'C'}, d{'D'}, e{'E'}, f{'F'}, g{'G'}, h{'H'}, i{'I'}, j{'J'};
-    AttrDependencies F;
-    F[a].insert(i);
-    F[Attrs{a,b}].insert(c);
-    F[Attrs{a,d}].insert(Attrs{g,h});
-    F[Attrs{b,d}].insert(Attrs{e,f});
-    F[h].insert(j);
+    /** Read Input **/
+    while( std::cin >> holder ){
+        switch(holder){
+            case 'R':
+                if( status&1 ){
+                    std::cout << "R is defined twice" << std::endl;
+                    return 0;
+                }
+                std::cin >> holder >> holder >> inp;
+                if(inp != "empty")
+                    for(auto &c : inp) R.insert(Attr {c});
 
-    Attrs a_closure = attr_closure(Attrs{b,d}, F); // = {B, D, E, F}
-    AttrDependencies s_closure = {};
-    SetAttrs cand_keys = candidate_keys(Attrs{a,b,c,d,e,f,g,h,i,j}, F); // = { A, B, D }
-    */
+                status |= 1;
+                break;
 
-    /** Set 4 **/
-    /*
-    Attr a{'A'}, b{'B'}, c{'C'}, d{'D'}, e{'E'}, f{'F'}, g{'G'}, h{'H'};
-    AttrDependencies F;
-    F[a].insert(Attrs{b,c});
-    F[c].insert(d);
-    F[d].insert(g);
-    F[e].insert(a);
-    F[e].insert(h);
-    F[h].insert(e);
+            case 'A':
+                if( status&2 ){
+                    std::cout << "A is defined twice" << std::endl;
+                    return 0;
+                }
+                std::cin >> holder >> holder >> inp;
+                if(inp != "empty")
+                    for(auto &c : inp) A.insert(Attr {c});
 
-    Attrs a_closure = attr_closure(Attrs{a,c}, F); // = {A, B, C, D, G}
-    AttrDependencies s_closure = {};
-    SetAttrs cand_keys = candidate_keys(Attrs{a,b,c,d,e,f,g,h}, F); // = {E, F}{F, H}
-    */
+                status |= 2;
+                break;
 
-    /** Set 5 **/
+            case 'F':
+                if( status&4 ){
+                    std::cout << "F is defined twice" << std::endl;
+                    return 0;
+                }
+                std::cin >> holder >> holder >> inp;
+                if(inp != "empty"){
+                    int ndep = stoi(inp);
+                    for(int i=0; i<ndep; i++){
+                        std::cin >> inp >> holder >> holder >> inpaux;
+                        Attrs left{}, right{};
+                        for(auto &c : inp) left.insert(Attr {c});
+                        for(auto &c : inpaux) right.insert(Attr {c});
+                        F[left].insert(right);
+                    }
+                }
 
-    Attr a{'A'}, b{'B'}, c{'C'}, d{'D'}, e{'E'}, f{'F'}, g{'G'};
-    AttrDependencies F;
-    F[a].insert(f);
-    F[a].insert(g);
-    F[b].insert(e);
-    F[c].insert(d);
-    F[d].insert(b);
-    F[e].insert(a);
-    F[Attrs{f,g}].insert(c);
-
-    Attrs a_closure = attr_closure(Attrs{f,g}, F); // = {A, B, C, D, E, F, G}
-    AttrDependencies s_closure = {}; //set_closure(Attrs{a,b,c,d,e,f,g}, F);
-    SetAttrs cand_keys = candidate_keys(Attrs{a,b,c,d,e,f,g}, F); // = {A}{B}...{E}{F, G}
+                status |= 4;
+                break;
+        }
+    }
 
 
-    /** Set TEST1 **/
-    /*
-    Attr a{'A'}, b{'B'}, c{'C'}, d{'D'};
-    AttrDependencies F;
-    F[a].insert(b);
-    F[b].insert(c);
-
-    Attrs a_closure = {};
-    AttrDependencies s_closure = set_closure(Attrs{a,b,c,d}, F);
-    SetAttrs cand_keys = {};
-    */
-
-
+    /** Attribute closure **/
+    Attrs a_closure = attr_closure(A, F);
     std::cout << "A+ closure: ";
-    for(auto &elem : a_closure) std::cout << elem << ' ';
-    std::cout << std::endl;
+    if(a_closure.empty()) std::cout << "empty" << std::endl;
+    else {
+        for(auto &elem : a_closure) std::cout << elem;
+        std::cout << std::endl;
+    }
 
-    std::cout << "F+ size: " << countDependencies(s_closure) << std::endl;
-    /*for(auto &f : s_closure){
-        std::cout << f.first << " -> ";
-        for(auto &e : f.second) std::cout << e << ' ';
-        std::cout << '\n';
-    }*/
 
+    /** Set closure **/
+    if((argc >= 3) and (strcmp(argv[2], "-countFD") == 0)){
+        AttrDependencies s_closure = set_closure(R,F);
+        std::cout << "F+ size: " << countDependencies(s_closure) << std::endl;
+    }
+
+
+    /** Candidate keys **/
+    SetAttrs cand_keys = candidate_keys(R, F);
     std::cout << "Candidate keys: \n";
     for(auto &attrs : cand_keys){
-        for(auto &e : attrs)
-            std::cout << e << ' ';
+        for(auto &c : attrs) std::cout << c;
         std::cout << '\n';
     }
 
